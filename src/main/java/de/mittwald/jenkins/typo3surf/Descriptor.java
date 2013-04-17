5b3004fef7e3b51664acb46b234792edcd78d5eb
Martin Helmich <m.helmich@mittwald.de>
2013-04-17 16:35:18 +0200

    [TASK] Added GPL licence comments.

--- src/main/java/de/mittwald/jenkins/typo3surf/Descriptor.java
+++ src/main/java/de/mittwald/jenkins/typo3surf/Descriptor.java
@@ -1,5 +1,25 @@
 package de.mittwald.jenkins.typo3surf;
 
+
+/*
+ * TYPO3 Surf Builder for Jenkins
+ * Copyright (C) 2013  Martin Helmich <m.helmich@mittwald.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 import hudson.model.AbstractProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
@@ -12,15 +32,39 @@ import javax.servlet.ServletException;
 import java.io.File;
 import java.io.IOException;
 
+
+
+/**
+ * Descriptor class for TYPO3 Surf build steps.
+ *
+ * @author Martin Helmich <m.helmich@mittwald.de>
+ */
 public class Descriptor extends BuildStepDescriptor<Builder>
 {
-    private String surfPath = "/var/surf";
+    /**
+     * Path to TYPO3 Surf installation (root directory, NOT the executable!)
+     */
+    protected String surfPath = "/var/surf";
 
 
-    private String surfContext = "Production";
+    /**
+     * TYPO3 Flow context to be used for Surf execution.
+     */
+    protected String surfContext = "Production";
 
 
 
+    /**
+     * Validate the TYPO3 Surf path.
+     *
+     * Checks if a "flow" executable exists in the given path and if the TYPO3.Surf package is installed.
+     *
+     * @param value The given TYPO3 Surf path.
+     * @return OK, if the given path is a valid TYPO3 Surf installation, otherwise ERROR.
+     *
+     * @throws IOException
+     * @throws ServletException
+     */
     public FormValidation doCheckSurfPath(@QueryParameter String value)
             throws IOException, ServletException
     {
@@ -38,6 +82,15 @@ public class Descriptor extends BuildStepDescriptor<Builder>
 
 
 
+    /**
+     * Validates the TYPO3 Flow context (must be either "Production" or "Development").
+     *
+     * @param value The Flow context to validate.
+     * @return OK if valid, otherwise ERROR.
+     *
+     * @throws IOException
+     * @throws ServletException
+     */
     public FormValidation doCheckSurfContext(@QueryParameter String value) throws IOException, ServletException
     {
         if (!value.equals("Development") && !value.equals("Production"))
--- src/main/java/de/mittwald/jenkins/typo3surf/Typo3SurfBuilder.java
+++ src/main/java/de/mittwald/jenkins/typo3surf/Typo3SurfBuilder.java
@@ -1,5 +1,25 @@
 package de.mittwald.jenkins.typo3surf;
 
+
+/*
+ * TYPO3 Surf Builder for Jenkins
+ * Copyright (C) 2013  Martin Helmich <m.helmich@mittwald.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.Launcher;
@@ -14,10 +34,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
+
+
+/**
+ * Builder for TYPO3 Surf deployments.
+ *
+ * @author Martin Helmich <m.helmich@mittwald.de></m.helmich@mittwald.de>
+ */
 public class Typo3SurfBuilder extends Builder
 {
 
-    private final String deployment;
+    protected final String deployment;
 
 
     @Extension
@@ -37,9 +64,6 @@ public class Typo3SurfBuilder extends Builder
 
 
 
-    /**
-     * We'll use this from the <tt>config.jelly</tt>.
-     */
     public String getDeployment()
     {
         return deployment;
